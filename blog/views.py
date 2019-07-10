7a82e170ac3c3a33f218ef851eee998420d9375e
daveman00 <czyrny.dawid@gmail.com>
2019-07-10 14:28:09 +0200

    remove blog_detail view as it breaks integer category view

--- blog/views.py
+++ blog/views.py
@@ -23,24 +23,25 @@ def blog_category(request, category):
     }
     return render(request, 'blog_category.html', context)
 
-def blog_detail(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    form = CommentForm()
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = Comment(
-                author=form.cleaned_data['author'],
-                body=form.cleaned_data['body'],
-                post=post,
-            )
-            comment.save()
-
-    comments = Comment.objects.filter(post=post)
-    context = {
-        'post': post,
-        'comments': comments,
-        'form': form,
-    }
-    return render(request, 'blog_detail.html', context)
+# this part causes the integer categories like (2019,1999) to break
+#def blog_detail(request, pk):
+#    post = Post.objects.get(pk=pk)
+#
+#    form = CommentForm()
+#    if request.method == 'POST':
+#        form = CommentForm(request.POST)
+#        if form.is_valid():
+#            comment = Comment(
+#                author=form.cleaned_data['author'],
+#                body=form.cleaned_data['body'],
+#                post=post,
+#            )
+#            comment.save()
+#
+#    comments = Comment.objects.filter(post=post)
+#    context = {
+#        'post': post,
+#        'comments': comments,
+#        'form': form,
+#    }
+#    return render(request, 'blog_detail.html', context)
